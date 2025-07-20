8af5bbc0f4569104fdefd85d828e577ff9c75c89
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-07-20 19:25:53 +0530

    default  value to cookie properties

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -44,8 +44,8 @@ authRouter.post("/login", async (req, res) => {
       const isLocalhost = process.env.FRONTEND_URL === "http://localhost:5173";
 
       res.cookie("token", token, {
-        httpOnly: !isLocalhost,
-        secure: !isLocalhost,
+        httpOnly: false,
+        secure: true,
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
