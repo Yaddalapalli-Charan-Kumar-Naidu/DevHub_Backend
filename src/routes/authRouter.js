bcf746e233292f31511a5cccc2c27d5e4c21b253
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-07-20 22:11:35 +0530

    removed extra option in cookie

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -44,11 +44,8 @@ authRouter.post("/login", async (req, res) => {
       const isLocalhost = process.env.FRONTEND_URL === "http://localhost:5173";
 
      res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,
+        secure: true,
   sameSite: "None",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-  // Do NOT set domain
 });
 
 
