afa3b79b9d3206d611e03e5515beb3d1972e1fd7
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-07-20 22:56:34 +0530

    importing jwt in authentication

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -5,7 +5,7 @@ import validator from "validator";
 import { signupValidator } from "../utils/validators.js";
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 authRouter.post("/signup", async (req, res) => {
   try {
     signupValidator(req);
@@ -42,11 +42,12 @@ authRouter.post("/login", async (req, res) => {
     if (isValidPassword) {
       const token = await user.getJWT();
       const isLocalhost = process.env.FRONTEND_URL === "http://localhost:5173";
-
-     res.cookie("token", token, {
-  sameSite: "None",
-
-});
+      res.cookie("token", token, {
+        httpOnly: !isLocalhost,
+        secure: !isLocalhost,
+        sameSite: isLocalhost ? "Lax" : "None",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
 
 
 
@@ -73,10 +74,12 @@ authRouter.get("/profile", authenticate, (req, res) => {
   }
 });
 authRouter.get("/me", (req, res) => {
-  const token = req.cookies.token;
+  const {token}=req.cookies;
+  console.log("Token:",token);
   if (!token) return res.status(401).json({ message: "Unauthorized" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded:",decoded);
     res.json({ user: decoded });
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
