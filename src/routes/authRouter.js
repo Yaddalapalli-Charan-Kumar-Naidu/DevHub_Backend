d71b12cf86f47dd10408e756aa4ae913ca505d4e
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-07-20 21:59:30 +0530

    changed for render frontend

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -43,12 +43,14 @@ authRouter.post("/login", async (req, res) => {
       const token = await user.getJWT();
       const isLocalhost = process.env.FRONTEND_URL === "http://localhost:5173";
 
-      res.cookie("token", token, {
-        httpOnly: !isLocalhost,
-        secure: !isLocalhost,
-        sameSite: isLocalhost ? "Lax" : "None",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      });
+     res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  // Do NOT set domain
+});
+
 
 
       res.status(200.).json({ "msg": "Login succesful", "data": user });
